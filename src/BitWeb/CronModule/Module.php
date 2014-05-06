ca24169f96624090c46e08659a4444426ab02981
Rain Ramm <rammrain@gmail.com>
2014-05-06 14:24:19 +0300

    Fix Module.php
    Also add tests

--- src/BitWeb/CronModule/Module.php
+++ src/BitWeb/CronModule/Module.php
@@ -8,15 +8,16 @@ class Module
 {
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../../../config/module.config.php';
     }
 
     public function getAutoloaderConfig()
     {
+        $dir = dirname(dirname(dirname(__DIR__)));
         return [
             'Zend\Loader\StandardAutoloader' => [
                 'namespaces' => [
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                    __NAMESPACE__ => $dir . '/src/' . __NAMESPACE__,
                 ],
             ],
         ];
