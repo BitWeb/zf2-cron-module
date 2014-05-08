dfb54b98ad5f97c69318fc402ecbc7d3ce218ba2
Rain Ramm <rammrain@gmail.com>
2014-05-08 11:59:31 +0300

    Fix error in cofiguration

--- config/controllers.config.php
+++ config/controllers.config.php
@@ -2,12 +2,12 @@
 
 return [
     'invokables' => [
-        'CronModule\Controller\Index' => 'CronModule\Controller\IndexController'
+        'BitWeb\CronModule\Controller\Index' => 'BitWeb\CronModule\Controller\IndexController'
     ],
     'initializers' => [
         function ($controller, \Zend\Mvc\Controller\ControllerManager $cm) {
             if ($controller instanceof \BitWeb\CronModule\Service\Cron\CronServiceAwareInterface) {
-                $controller->setCronService($cm->getServiceLocator()->get('CronModule\Service\Cron'));
+                $controller->setCronService($cm->getServiceLocator()->get('BitWeb\CronModule\Service\Cron'));
             }
 
             return $controller;
