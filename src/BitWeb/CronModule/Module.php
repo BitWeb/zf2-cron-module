2c83e8b8dc69d5a1d598ea44d7adcb85176bf7ad
Doron Gutman <doron@gutman.me>
2015-02-06 00:25:54 +0200

    moved the controllers config to the Module file as it is using closure functions

--- src/BitWeb/CronModule/Module.php
+++ src/BitWeb/CronModule/Module.php
@@ -3,12 +3,32 @@
 namespace BitWeb\CronModule;
 
 use Zend\Console\Adapter\AdapterInterface;
+use Zend\ModuleManager\Feature\ControllerProviderInterface;
 
-class Module
+class Module implements ControllerProviderInterface
 {
     public function getConfig()
     {
         return include __DIR__ . '/../../../config/module.config.php';
+    }
+
+    public function getControllerConfig()
+    {
+        return [
+            'invokables' => [
+                'BitWeb\CronModule\Controller\Index' => 'BitWeb\CronModule\Controller\IndexController'
+            ],
+            'initializers' => [
+                function ($controller, \Zend\Mvc\Controller\ControllerManager $cm) {
+                    if ($controller instanceof \BitWeb\CronModule\Service\Cron\CronServiceAwareInterface) {
+                        $controller->setCronService($cm->getServiceLocator()->get('BitWeb\CronModule\Service\Cron'));
+                    }
+
+                    return $controller;
+                }
+            ]
+
+        ];
     }
 
     public function getAutoloaderConfig()
